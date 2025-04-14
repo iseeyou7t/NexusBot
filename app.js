3be53feef669ddf85cf3725afafb59255910ae6d
iseeyou7t <tahabarakat16@gmail.com>
2025-04-13 21:50:37 -0400

    Update app.js
    Signed-off-by: iseeyou7t <tahabarakat16@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Configuration
 const BOT_CLIENT_ID = "1321862179418411150";
 const PERMISSIONS = "8"; // Administrator permissions
-const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${BOT_CLIENT_ID}&permissions=${PERMISSIONS}&scope=bot%20applications.commands`;
+const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=1321862179418411150&permissions=8&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord%2Fcallback&integration_type=0&scope=identify+connections+guilds.join+guilds.channels.read+bot+rpc.notifications.read+rpc.voice.read+rpc+gdm.join+guilds.members.read+guilds+email+rpc.voice.write+rpc.video.write+rpc.screenshare.write+webhook.incoming+messages.read+rpc.activities.write+rpc.screenshare.read+rpc.video.read+applications.builds.upload+applications.commands+applications.entitlements+activities.write+relationships.read+voice+role_connections.write+presences.write+dm_channels.messages.read+gateway.connect+payment_sources.country_code+sdk.social_layer+sdk.social_layer_presence+lobbies.write+account.global_name.update+openid+dm_channels.messages.write+presences.read+dm_channels.read+relationships.write+activities.invites.write+activities.read+applications.store.update+applications.builds.read`;
 
 document.getElementById('inviteBotBtn').addEventListener('click', () => {
     // Simple version - opens invite directly
